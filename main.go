59615e0ff7413578ed7330b4b1885c240522be10
Jigar Thacker <jigar.thacker@colortokens.com>
2022-09-24 13:52:56 +0530

    added short url create api

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
+	"urlshortner/cache"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -16,7 +19,9 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/", shorten)
+	e.POST("/shorturl", shorten)
+
+	fmt.Println(time.Now().String())
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
@@ -24,5 +29,19 @@ func main() {
 
 // Handler
 func shorten(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello from URL shortner!")
+
+	type In struct {
+		Url string `json:"url"`
+	}
+	in := In{}
+
+	c.Bind(&in)
+
+	urls := cache.Init()
+	surl, err := urls.CreateShortURL(in.Url)
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	return c.String(http.StatusOK, surl)
 }
