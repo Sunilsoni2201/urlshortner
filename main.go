c6e45720da7f7ffd5df696e1656d3487a8a68116
Jigar Thacker <jigar.thacker@colortokens.com>
2022-09-24 12:55:24 +0530

    added requirement and echo server init

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("Best URL Shortner")
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", shorten)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// Handler
+func shorten(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello from URL shortner!")
 }
